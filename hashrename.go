7c7cb6fcc45e1c041c26a8cacab434283f21c509
Brandon Pitman <brandon.pitman@gmail.com>
2020-07-20 16:02:26 -0700

    Add concurrency, allow hash function to be selected.
    The default hash function changes from SHA1 to SHA512/256. This is a
    more modern, efficient, and secure cryptographic hash. It's also twice
    as long, but that's OK for my purposes.

--- hashrename.go
+++ hashrename.go
@@ -2,25 +2,93 @@ package main
 
 import (
 	"crypto/sha1"
+	"crypto/sha512"
 	"encoding/hex"
 	"flag"
 	"fmt"
+	"hash"
 	"io"
 	"os"
 	"path/filepath"
+	"runtime"
+	"sync"
+	"sync/atomic"
 )
 
 var (
-	dryRun = flag.Bool("dry_run", false, "If set, do not rename files, just print what renames would occur.")
+	dryRun      = flag.Bool("dry_run", false, "If set, do not rename files, just print what renames would occur.")
+	concurrency = flag.Int("concurrency", 0, "The number of files to process at once. If unset, a reasonable value will be chosen automatically.")
+	hashName    = flag.String("hash", "sha512_256", "The hash to use. Supported values include `sha1` & `sha512_256`.")
 )
 
 func main() {
+	// Parse & validate flags.
 	flag.Parse()
 	if len(flag.Args()) == 0 {
-		die("Usage: hashrename [--dry_run] globs")
+		die("Usage: hashrename [--dry_run] [--concurrency=N] [--hash=sha512_256] globs")
+	}
+	switch {
+	case *concurrency == 0:
+		*concurrency = runtime.GOMAXPROCS(0)
+	case *concurrency < 0:
+		die("The --concurrency flag must be non-negative.")
+	}
+	var newHash func() hash.Hash
+	switch *hashName {
+	case "sha1":
+		newHash = sha1.New
+	case "sha512_256":
+		newHash = sha512.New512_256
+	default:
+		die("Unknown --hash value %q", *hashName)
 	}
 
-	// Find files to rename.
+	// Start per-file workers.
+	var wg sync.WaitGroup
+	var errCount int64
+	ch := make(chan string)
+	for i := 0; i < *concurrency; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			hash := newHash()
+			for fn := range ch {
+				if err := func() error {
+					hash.Reset()
+					f, err := os.Open(fn)
+					if err != nil {
+						return fmt.Errorf("couldn't open: %w", err)
+					}
+					defer f.Close()
+					if _, err := io.Copy(hash, f); err != nil {
+						return fmt.Errorf("couldn't read: %w", err)
+					}
+					if err := f.Close(); err != nil {
+						return fmt.Errorf("couldn't close: %w", err)
+					}
+
+					newFn := hex.EncodeToString(hash.Sum(nil))
+					ext := filepath.Ext(fn)
+					if ext != "" {
+						newFn = fmt.Sprintf("%s%s", newFn, ext)
+					}
+					newFn = filepath.Join(filepath.Dir(fn), newFn)
+					fmt.Printf("%s -> %s\n", fn, newFn)
+					if !*dryRun {
+						if err := os.Rename(fn, newFn); err != nil {
+							return fmt.Errorf("couldn't rename: %w")
+						}
+					}
+					return nil
+				}(); err != nil {
+					atomic.AddInt64(&errCount, 1)
+					fmt.Fprintf(os.Stderr, "Couldn't handle %q: %v\n", fn, err)
+				}
+			}
+		}()
+	}
+
+	// Find files to rename. (find all files before renaming anything to ensure we handle each file only once)
 	files := map[string]struct{}{}
 	for _, glob := range flag.Args() {
 		fns, err := filepath.Glob(glob)
@@ -32,35 +100,13 @@ func main() {
 		}
 	}
 	fmt.Printf("Renaming %d file(s)\n", len(files))
-
-	// Compute sha1sums.
-	// TODO: do this in parallel? (may not be helpful; typically, this loop is IO-bound)
-	hash := sha1.New()
 	for fn := range files {
-		hash.Reset()
-		f, err := os.Open(fn)
-		if err != nil {
-			die("Couldn't open %q: %v", fn, err)
-		}
-		if _, err := io.Copy(hash, f); err != nil {
-			die("Couldn't read %q: %v", fn, err)
-		}
-		if err := f.Close(); err != nil {
-			die("Couldn't close %q: %v", fn, err)
-		}
-
-		newFn := hex.EncodeToString(hash.Sum(nil))
-		ext := filepath.Ext(fn)
-		if ext != "" {
-			newFn = fmt.Sprintf("%s%s", newFn, ext)
-		}
-		newFn = filepath.Join(filepath.Dir(fn), newFn)
-		fmt.Printf("%s -> %s\n", fn, newFn)
-		if !*dryRun {
-			if err := os.Rename(fn, newFn); err != nil {
-				fmt.Fprintf(os.Stderr, "Couldn't rename %q to %q: %v\n", fn, newFn, err)
-			}
-		}
+		ch <- fn
+	}
+	close(ch)
+	wg.Wait()
+	if errCount > 0 {
+		die("Encountered %d errors", errCount)
 	}
 }
 
